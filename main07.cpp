fcdaf7e93eb0b222595d72631dcc70a6a5db4691
TheProgrammer565 <76843640+TheProgrammer565@users.noreply.github.com>
2021-05-19 14:57:33 +0530

    Find the ascii value

---
+++ main07.cpp
@@ -0,0 +1,13 @@
+//Find the ascii value 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	char ch;
+	cout<<"Enter a character: ";
+	cin>>ch;
+	cout<<"ASCII value of "<<ch<<" is:"<<int(ch);	
+	return 0;
+}
+
