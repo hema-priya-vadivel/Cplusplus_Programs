9bb39d7e929487b809216da3044216986ec55e92
TheProgrammer565 <76843640+TheProgrammer565@users.noreply.github.com>
2021-05-19 14:54:30 +0530

    Sum of two numbers

---
+++ main03.cpp
@@ -0,0 +1,17 @@
+//Sum of two numbers
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int firstNum,secondNum,sum,product;
+	cout<<"Enter the value of First Number: ";
+	cin>>firstNum;
+	cout<<"Enter the value of Second Number: ";
+	cin>>secondNum;
+	sum=firstNum+secondNum;
+	cout<<"The sum of two numbers is: "<<sum<<"\n";
+	product=firstNum*secondNum;
+	cout<<"The product of two numbers is: "<<product;
+	return 0;
+}
