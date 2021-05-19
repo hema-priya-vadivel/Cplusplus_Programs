f19ced2e6c683727a2056a558c90c581449e5362
TheProgrammer565 <76843640+TheProgrammer565@users.noreply.github.com>
2021-05-19 14:53:59 +0530

    Find the number entered by user

---
+++ main02.cpp
@@ -0,0 +1,12 @@
+//Find the number entered by user
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int num;
+	cout<<"Enter a number: ";
+	cin>>num;
+	cout<<"The entered number is: "<<num;
+	return 0;
+}
